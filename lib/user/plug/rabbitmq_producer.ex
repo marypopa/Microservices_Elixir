14e4049104f79fd81d4a831aca259ce9b2c8a0e5
Maria Popa <maria.popasb@gmail.com>
2020-06-05 16:47:31 +0300

    clean priducer file

--- lib/user/plug/rabbitmq_producer.ex
+++ lib/user/plug/rabbitmq_producer.ex
@@ -1,10 +1,6 @@
 defmodule User.Plug.RabbitmqProducer do
 
     def send_message(user) do
-    # hostname = System.get_env("RABBITMQ_HOST")
-    # username = System.get_env("RABBITMQ_DEFAULT_USER")
-    # password = System.get_env("RABBITMQ_DEFAULT_PASS")
-    # options = [host: hostname, port: 5672, virtual_host: "/", username: username, password: password]
     {:ok, connection} = AMQP.Connection.open("amqp://tfavcusn:vr7hadAWY3fVvmTMgUJ-VwDsf_otp7yU@kangaroo.rmq.cloudamqp.com/tfavcusn", :undefined)
     {:ok, channel} = AMQP.Channel.open(connection)
     AMQP.Queue.declare(channel, "gen_server_test_queue", durable: true)
